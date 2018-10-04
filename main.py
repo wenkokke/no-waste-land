a38e8a93efefbca5cb18a4402b28669f02e4a1bf
Wen Kokke <wenkokke@users.noreply.github.com>
2018-10-04 13:12:37 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,98 @@
+import argparse
+import collections
+import json
+import markovify
+import random
+import re
+import string
+import operator
+import os.path
+
+my_notebook='params.data'
+
+# when pals talk to me, I like it if they use simple words. I am not very good
+# at difficult words. I like it when they ask me to write things, to critique
+# things, or to look at what they wrote. when they want me to look at what they
+# wrote, it's best if they ask me to 'study' it.
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+group.add_argument("--write", action="store_true", help="write a pome")
+group.add_argument("--critique", type=str, help="rate a pome between 0-1")
+group.add_argument("--study", type=str, help="learn from new pomes")
+args = parser.parse_args()
+
+class PrettyPomeMaker(markovify.Text):
+    word_split_pattern = re.compile(r"[-.,:;?!]|\w+")
+    def word_split(self, sentence):
+        return re.findall(self.word_split_pattern, sentence)
+    sentence_split_pattern = re.compile(r"\s*[.\n]+\s*")
+    def sentence_split(self, text):
+        return re.split(self.sentence_split_pattern, text)
+
+def make_it_better(letters, my_fav_letter):
+    return ''.join(
+        letter if letter in ' -.,:;?!' else my_fav_letter
+        for letter in letters
+        if letter in string.printable)
+
+def punctuation_is_important(letters):
+    letters = re.sub(r' ([.,:;?!])',r'\1',letters)
+    if not letters[-1] in '-.,:;?!':
+        letters += '.'
+    return letters
+
+def punctuation_is_important_but_not_my_favorite(letters):
+    return re.sub(r'[-.,:;?!\n\s]','',letters)
+
+def what_is_cool(letters):
+    counter = collections.Counter(
+        filter(lambda letter: letter in string.printable,
+               punctuation_is_important_but_not_my_favorite(
+                   letters)))
+    return sorted(counter.items(), key=operator.itemgetter(1), reverse=False)[0][0]
+
+# to write a pome, first I read my notes from my notebook -- I like to write
+# down what my favorite letter is, and what some of my favorite words are,
+# because I'm not very good at remembering that -- and then I pick some of my
+# favorite words and make them into a nice pome. I think that three to five
+# lines is a good length for a pome, and nothing will ever change my mind about
+# this. I think good punctuation is important for a pome.
+if args.write:
+    with open(my_notebook,'r') as notebook:
+        howto_pome = markovify.Text.from_json(notebook.read())
+
+    title = punctuation_is_important(howto_pome.make_sentence())
+    content = ''
+    for _ in range(random.randint(3,5)):
+        sentence = punctuation_is_important(howto_pome.make_sentence())
+        if sentence:
+            content += sentence
+
+    print(json.dumps({ 'title': title, 'content': content }))
+
+# when you look at a pome and you try to say how good that pome is, it's
+# important to look at how it's written. when pomes use my favorite letter,
+# that makes me happy.
+if args.critique:
+    with open(my_notebook,'r') as notebook_file:
+        my_fav_letter = json.loads(notebook_file.read())['my_fav_letter']
+
+    letters = punctuation_is_important_but_not_my_favorite(args.critique)
+    points = collections.Counter(letters)[my_fav_letter] / float(len(letters))
+
+    print(json.dumps({ 'score': points, }))
+
+# sometimes, when I read a pome written by a pome pal of mine, I go "oh wow,
+# this is amazing, just look at that letter!" and then I really want to write
+# some pomes and use what I learned.
+if args.study:
+    new_fav_letter = what_is_cool(args.study)
+    letters = make_it_better(args.study, new_fav_letter)
+    howto_pome = PrettyPomeMaker(letters, retain_original=False)
+
+    with open(my_notebook,'w') as notebook_file:
+        notebook_dict = howto_pome.to_dict()
+        notebook_dict['my_fav_letter'] = new_fav_letter
+        notebook_file.write(json.dumps(notebook_dict))
+
+    print(json.dumps({ 'success': True, }))
